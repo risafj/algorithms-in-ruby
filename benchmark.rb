723ad2c011341b54a092ebcd22e70027e6ea9726
Risa Fujii <risa@kray.jp>
2019-10-22 14:26:07 +0900

    Add benchmark

---
+++ benchmark.rb
@@ -0,0 +1,10 @@
+require 'benchmark'
+require_relative 'binary_search'
+require_relative 'serial_search'
+require_relative 'find_method'
+
+Benchmark.bm(7) do |x|
+  x.report('serial') {serial_search(id: 10_000)}
+  x.report('binary') {binary_search(id: 10_000)}
+  x.report('find') {find_by_id(id: 10_000)}
+end
