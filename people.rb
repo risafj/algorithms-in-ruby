41992f057d2cb81e64a2968fd81c73bf8b8e2f6d
Risa Fujii <risa@kray.jp>
2019-10-22 13:39:43 +0900

    Use Faker to create 100,000 people

--- people.rb
+++ people.rb
@@ -1,9 +1,7 @@
-PEOPLE = [
-  { id: 1, name: 'Bojack' },
-  { id: 2, name: 'Diane' },
-  { id: 3, name: 'Mr. Peanutbutter' },
-  { id: 4, name: 'Princess Carolyn' },
-  { id: 5, name: 'Hollyhock' },
-  { id: 6, name: 'Judah'},
-  { id: 7, name: 'Ralph'}
-].freeze
+require 'faker'
+
+PEOPLE = []
+
+(1..100_000).each do |n|
+  PEOPLE << {id: n, name: Faker::Name.name }
+end
