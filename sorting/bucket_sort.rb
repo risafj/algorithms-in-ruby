0c62f83209c694366d1d62f84cb78e8e8ad08e7c
Risa Fujii <risa@kray.jp>
2019-10-25 09:55:23 +0900

    Cleanup buckets creation

--- sorting/bucket_sort.rb
+++ sorting/bucket_sort.rb
@@ -2,32 +2,28 @@ require_relative 'numbers'
 require 'pry'
 
 def bucket_sort(bucket_number = 5)
-  interval = (@numbers.max - @numbers.min)/bucket_number
-
-  buckets = []
-  bucket_number.times do |n|
-    min = @numbers.min + interval * n
-    # If last bucket, max should be the max number. Otherwise, add the interval.
-    max = n == bucket_number - 1 ? @numbers.max : (@numbers.min + interval * (n + 1) - 1)
-    buckets << Bucket.new(min: min, max: max)
-  end
-
-  statement = []
-  buckets.each do |b|
-    statement << "minimum is #{b.min}, maximum is #{b.max}"
-  end
-
-  p "min is #{@numbers.min}"
-  p "max is #{@numbers.max}"
-  p "interval is #{interval}"
-  p statement
+  buckets = Bucket.create_buckets(bucket_number: bucket_number, min_number: @numbers.min, max_number: @numbers.max)
 end
+
 class Bucket
   attr_reader :min, :max
 
   def initialize(min:, max:)
     @min = min
     @max = max
+  end
+
+  def self.create_buckets(bucket_number:, min_number:, max_number:)
+    interval = (max_number - min_number)/bucket_number
+
+    buckets = []
+    bucket_number.times do |n|
+      min = min_number + interval * n
+      # If last bucket, max should be the max number. Otherwise, add the interval.
+      max = n == bucket_number - 1 ? max_number : (min_number + interval * (n + 1) - 1)
+      buckets << Bucket.new(min: min, max: max)
+    end
+    buckets
   end
 end
 
