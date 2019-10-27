a0759831ba4c0e726eee2417e13f6a6f600af2ce
Risa Fujii <risa@kray.jp>
2019-10-27 11:02:38 +0900

    Update benchmark

--- sorting/benchmark.rb
+++ sorting/benchmark.rb
@@ -1,10 +1,12 @@
 require 'benchmark'
 require_relative 'bubble_sort'
 require_relative 'bucket_sort'
+require_relative 'insertion_sort'
 require_relative 'numbers'
 require 'pry'
 
-Benchmark.bm(7) do |x|
+Benchmark.bm(10) do |x|
   x.report('bubble') { BubbleSorter.new(@numbers).bubble_sort }
   x.report('bucket') { BucketSorter.new(array_of_numbers: @numbers).bucket_sort }
+  x.report('insertion') { InsertionSorter.new(@numbers).insertion_sort }
 end
