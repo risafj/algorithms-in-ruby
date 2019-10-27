bb7667fab0b011e6d4a98ba4632829f1d70f20a1
Risa Fujii <risa@kray.jp>
2019-10-27 11:02:28 +0900

    Add insertion sort

---
+++ sorting/insertion_sort.rb
@@ -0,0 +1,27 @@
+require_relative 'numbers'
+require 'pry'
+
+class InsertionSorter
+  attr_reader :array_of_numbers
+
+  def initialize(array_of_numbers)
+    @array_of_numbers = array_of_numbers
+  end
+
+  def insertion_sort
+    max_index = array_of_numbers.size - 1
+
+    (1..max_index).each do |current_index|
+      (0..(current_index - 1)).each do |search_index|
+        next if array_of_numbers[search_index] < array_of_numbers[current_index]
+
+        array_of_numbers.insert(search_index, array_of_numbers.delete_at(current_index))
+        break
+      end
+    end
+    array_of_numbers
+  end
+end
+
+# sorter = InsertionSorter.new(@numbers)
+# puts sorter.insertion_sort
