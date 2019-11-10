c57b4bb93b9ce70a5f21037c1beb93ce80da0f85
Risa Fujii <risa@kray.jp>
2019-11-10 11:33:56 +0900

    Add benchmark

--- sorting/benchmark.rb
+++ sorting/benchmark.rb
@@ -1,12 +1,14 @@
 require 'benchmark'
+require_relative 'numbers'
 require_relative 'bubble_sort'
 require_relative 'bucket_sort'
 require_relative 'insertion_sort'
-require_relative 'numbers'
+require_relative 'selection_sort'
 require 'pry'
 
 Benchmark.bm(10) do |x|
   x.report('bubble') { BubbleSorter.new(@numbers).bubble_sort }
   x.report('bucket') { BucketSorter.new(array_of_numbers: @numbers).bucket_sort }
   x.report('insertion') { InsertionSorter.new(@numbers).insertion_sort }
+  x.report('selection') { SelectionSorter.new(@numbers).selection_sort }
 end
