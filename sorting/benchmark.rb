16a92a51468e8d7d77e0963128b0145186099322
Risa Fujii <risa@kray.jp>
2019-11-10 19:50:26 +0900

    Add benchmark

--- sorting/benchmark.rb
+++ sorting/benchmark.rb
@@ -4,6 +4,7 @@ require_relative 'bubble_sort'
 require_relative 'bucket_sort'
 require_relative 'insertion_sort'
 require_relative 'selection_sort'
+require_relative 'merge_sort'
 require 'pry'
 
 Benchmark.bm(10) do |x|
@@ -11,4 +12,5 @@ Benchmark.bm(10) do |x|
   x.report('bucket') { BucketSorter.new(array_of_numbers: @numbers).bucket_sort }
   x.report('insertion') { InsertionSorter.new(@numbers).insertion_sort }
   x.report('selection') { SelectionSorter.new(@numbers).selection_sort }
+  x.report('merge') { MergeSorter.new(@numbers).merge_sort }
 end
